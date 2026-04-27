ccd7654e61ee6b69ed5b9ef4317159f1e55c300f
lingraj1124 <82047703+lingraj1124@users.noreply.github.com>
2026-04-27 15:00:46 +0530

    Update HealthCheckController.java

--- src/main/java/com/example/webhook/controller/HealthCheckController.java
+++ src/main/java/com/example/webhook/controller/HealthCheckController.java
@@ -13,6 +13,6 @@ import java.util.Map;
 public class HealthCheckController {
     @GetMapping("/")
     public String health() {
-        return "✅ App is alive!";
+        return "✅ App is alive2!";
     }
 }
